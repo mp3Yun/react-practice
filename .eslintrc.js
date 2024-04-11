0a4eec1f938e798c3d250b85748a2bcf8e118504
irenelo <irenelo@hitrust.com.tw>
2024-04-11 17:36:11 +0800

    add format setting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended', // Make sure this is the last element in the array
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    // Add specific ESLint rules or override existing ones here
+  },
+};
