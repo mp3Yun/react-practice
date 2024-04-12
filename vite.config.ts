9496ae53188b351cf1c860dcd334c72833793c56
irenelo <irenelo@hitrust.com.tw>
2024-04-12 11:17:37 +0800

    add base list item

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/styles/global.scss";`, // 全域 SCSS 樣式文件
+      },
+    },
+  },
 })
