db510730b2b277cb464a0620c8354f8e4877d1b6
irenelo <irenelo@hitrust.com.tw>
2024-04-15 10:28:20 +0800

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,10 @@ function App() {
         iconSrc={IconDefinitions.faPencil}
         text="右 icon + 文字按鈕"
       ></TextIconBtn>
+      <br />
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-2xl font-bold">Hello, Tailwind CSS!</h1>
+      </div>
     </>
   )
 }
