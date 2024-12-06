c8368cc0d725fe7be84e1f880f4776b745da8d1b
irenelo <both9731@gmail.com>
2024-12-06 13:28:21 +0800

    delete css and not use

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import customTheme from "./theme.ts";
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import customTheme from './theme.ts'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <ChakraProvider theme={customTheme}>
     <App />
   </ChakraProvider>
-);
+)
