b5382a58b0c864cd376ba14ed7a832a6c8f8c65a
irenelo <both9731@gmail.com>
2024-12-27 10:17:37 +0800

    upgrade TanStack

--- src/feature/form/formRoute.tsx
+++ src/feature/form/formRoute.tsx
@@ -1,6 +1,12 @@
-import { createRoute } from '@tanstack/react-router'
+import { createRoute, RouteContext } from '@tanstack/react-router'
 import { routePathMaps, RoutePathEnum } from '../../dto/route-paths'
 import { homeRoute } from '../home/homeRoute'
+
+// Define a type for the context
+interface NavigationContext {
+  isDirty: boolean
+  confirmLeave: () => Promise<boolean>
+}
 
 export const formRoute = createRoute({
   getParentRoute: () => homeRoute,
@@ -18,6 +24,11 @@ export const formUseFieldArrayRoute = createRoute({
   getParentRoute: () => homeRoute,
   path: '/formUseFieldArray', // 定義路由的 path
   component: routePathMaps[RoutePathEnum.FormUseFieldArray].component, // 定義該路由的對應組件
+  // beforeLoad: ({ context }: { context: NavigationContext }) => {
+  //   if (context.isDirty) {
+  //     return context.confirmLeave()
+  //   }
+  // },
 })
 
 export const formTreeRoute = formRoute.addChildren([
