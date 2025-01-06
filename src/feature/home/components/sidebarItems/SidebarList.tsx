4a754d9ba80837b007e23320ec28d53a5af465cd
irenelo <both9731@gmail.com>
2025-01-06 17:10:30 +0800

    fix default expanded item of sidebarList

--- src/feature/home/components/sidebarItems/SidebarList.tsx
+++ src/feature/home/components/sidebarItems/SidebarList.tsx
@@ -6,7 +6,7 @@ import {
   AccordionPanel,
   Box,
 } from '@chakra-ui/react'
-import { Link, Route, useRouter } from '@tanstack/react-router'
+import { Link, useRouter } from '@tanstack/react-router'
 import React from 'react'
 
 type FormattedRoute = {
@@ -16,47 +16,6 @@ type FormattedRoute = {
   hasChildren: boolean
   children?: FormattedRoute[]
 }
-
-// const parserRoute = (routes: [string, unknown][]): FormattedRoute[] => {
-//   // 將路由轉換為基本格式
-//   const formattedRoutes: FormattedRoute[] = routes.map(([path]) => {
-//     const name = path.split('/').filter(Boolean).pop() || '' // 提取最後的路徑部分作為 name
-//     return { path, name, hasChildren: false, children: [] }
-//   })
-
-//   // 建立路由的樹狀結構
-//   const buildTree = (parentPath: string): FormattedRoute[] => {
-//     return formattedRoutes
-//       .filter(
-//         (route) =>
-//           route.path.startsWith(parentPath + '/') && route.path !== parentPath
-//       )
-//       .map((childRoute) => {
-//         const children = buildTree(childRoute.path) // 遞迴處理子路由
-//         return {
-//           ...childRoute,
-//           hasChildren: children.length > 0,
-//           children,
-//         }
-//       })
-//   }
-
-//   // 組裝最終結構
-//   const finalRoutes = formattedRoutes
-//     .filter((route) => !route.path.includes('/', 1)) // 篩選出根路徑（無父路徑的路由）
-//     .map((rootRoute, index) => {
-//       const children = buildTree(rootRoute.path)
-//       return {
-//         id: index + 1, // 唯一 ID
-//         ...rootRoute,
-//         hasChildren: children.length > 0,
-//         children,
-//       }
-//     })
-
-//   console.log(finalRoutes)
-//   return finalRoutes
-// }
 
 const parserRoute = (routes: [string, unknown][]): FormattedRoute[] => {
   // 將路由轉換為基本格式
@@ -112,6 +71,7 @@ const SidebarList: React.FC = () => {
 
   // 渲染子選單
   const renderChildren = (children: FormattedRoute[]) => {
+    // 展開狀態管理
     return children.map((child, index) => (
       <AccordionItem key={index}>
         <h3>
@@ -128,7 +88,7 @@ const SidebarList: React.FC = () => {
   }
 
   return (
-    <Accordion bg={'gray.50'} defaultIndex={[0]} allowMultiple flexDir={'row'}>
+    <Accordion bg={'gray.50'} defaultIndex={[-1]} allowMultiple flexDir={'row'}>
       {menu.map((item, index) => (
         <AccordionItem key={index}>
           <h2>
